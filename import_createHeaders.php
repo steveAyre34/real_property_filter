810175d9502a1f1287bcde1b1b31fe82ff85462d
Ray Borriello <rborriello2@gmail.com>
2017-02-24 10:32:18 -0500

    Importer partially working
    Importer working for tables other than owner

--- import_createHeaders.php
+++ import_createHeaders.php
@@ -1,6 +1,6 @@
 <?php
-	
-function chooseHeaders($importFilePath, $importFileName, $databaseTable) {
+
+function chooseHeaders($importFilePath, $importFileName, $databaseTable, $databaseTableHeaders, $missingHeaders) {
 	//mysqli_fetch_fields yields a number for field type. This map will allow us to replace the number.
 	$mysql_data_type_map = array(
 		1	=>	'tinyint',
@@ -26,29 +26,31 @@ function chooseHeaders($importFilePath, $importFileName, $databaseTable) {
 	$fileHeaders = fgets($importFile);
 	$fileHeaders = explode("\t", $fileHeaders);
 	
-	//echo "<!DOCTYPE html>"
-	echo "<html>";
-	echo "<head>";
-	echo "<link rel='stylesheet' type='text/css' href='import.css'>";
-	echo "</head>";
-		echo "<body>";
-			print("<b>File Headers:</b><br>");
-			print("These are the file headers for " . $databaseTable . ".txt. Please select a type and length for each field:<br><br>");
-			foreach($fileHeaders as $f) {
-				echo "<input type='text' name='headerName' id='headerName' value=\"" . $f . "\"/input>";
-				echo "<select name='headerType' id='headerType'/>";
-				echo "<option value='selected'>Choose a data type</option>";
-				foreach($mysql_data_type_map as $k => $v) {
-					echo "<option value='" . $v . "'>" . $v . "</option>";
+	if(empty($missingHeaders)) {
+		//echo "<!DOCTYPE html>"
+		echo "<html>";
+		echo "<head>";
+		echo "<link rel='stylesheet' type='text/css' href='import.css'>";
+		echo "</head>";
+			echo "<body>";
+				print("<b>File Headers:</b><br>");
+				print("These are the file headers for " . $databaseTable . ".txt. <br><br>");
+				echo "<form action='do_import_owner.php' method='POST' enctype='multipart/form-data'>";
+				foreach($fileHeaders as $f) {
+					echo "<input type='text' name='fieldName' id='fieldName' value=\"" . $f . "\"/>";
+					echo "<select name='fieldType' id='fieldType'/>";
+					echo "<option value='selected'>Choose a data type</option>";
+					foreach($mysql_data_type_map as $k => $v) {
+						echo "<option value='" . $v . "'>" . $v . "</option>";
+					}
+					echo "</select>";
+					echo "<input name='fieldLength' id='fieldLength' placeholder='Enter field length'></input><br><br>";
 				}
-				echo "</select>";
-				echo "<input name='headerLength' id='headerLength' placeholder='Enter field length'></input>";
-				echo "<input name='primaryKey' id='primaryKey' type='checkbox' value=\"" . $f . "\"></input><br><br>";
-			}
-			echo "<button type='submit' name='btn-upload'>Next</button>";
-			
-		echo "</body>";
-	echo "</html>";
+				echo "<button type='submit' name='btn-upload'>Next</button>";
+			echo "</form>";
+			echo "</body>";
+		echo "</html>";
+	
 	
 	/*if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
@@ -58,7 +60,7 @@ function chooseHeaders($importFilePath, $importFileName, $databaseTable) {
 		
 		/*Creating the insert statement
 		*/
-		$insertStatement = "LOAD DATA INFILE '" . $importFileName . "' INTO TABLE " . $databaseTable . " FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\n' IGNORE 1 LINES(";
+		/*$insertStatement = "LOAD DATA INFILE '" . $importFileName . "' INTO TABLE " . $databaseTable . " FIELDS TERMINATED BY '\\t' LINES TERMINATED BY '\\n' IGNORE 1 LINES(";
 		
 		//Now append the headers to the LOAD DATA INFILE statement, in the order they were retrieved from the file
 		//By structuring the query this way, even if the order of headers in the file changes the data can still be inserted
@@ -72,7 +74,6 @@ function chooseHeaders($importFilePath, $importFileName, $databaseTable) {
 		//Now we specify the values to be inserted
 		$insertStatement .= ");"; 
 		
-		return $insertStatement;
-	}*/
-}
+		return $insertStatement;*/
+	}
 ?> 
