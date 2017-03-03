c7919f9be178f02580fcde853fdecc0f1f177a02
Ray Borriello <rborriello2@gmail.com>
2017-03-03 00:59:18 -0500

    Choose header page displaying correctly, routing working. Need to implement do_import_owner and have non-matching table rows highlight on field change.

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 	require('common.php');
+
+	/*if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		if(in_array('owner.txt', $_FILES['uploadFile']['name'])) {
+			echo file_get_contents('https://localhost/real_property_filter/createHeaders.php');
+		}
+	}*/
+		
 ?>
 <!DOCTYPE html>
 
@@ -7,14 +14,16 @@
 <head>
 <title>Welcome to the New York State Real Property Data Filter</title>
 <!--<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">-->
-<meta charset="UTF-8">
+<meta content="text/html;charset=utf-8" http-equiv-"Content-Type">
+<meta content="utf-8" http-equiv="encoding">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 
 <body bgcolor="#b3d5ff">
 <h1>Welcome to the New York State Real Property Data Filter</h1>
 
-<form id="form" action="createHeaders.php" method="GET" enctype="multipart/form-data">
+<form id="form" action="do_import.php" method="POST" enctype="multipart/form-data">
+
 <p><strong>Please Select A County:</strong></p>
 <select name="county" id="chosenCounty">
 <?php
